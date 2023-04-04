df7f502bd899a181e68dd9a47f702ce3ed3f640d
Jonatan Lagland <e1700814@edu.vamk.fi>
2023-04-04 17:19:54 +0300

    Mobiilimaksusovellus

--- app/src/main/java/vamk/e1700814/payapplication/api/SessionInterface.java
+++
@@ -1,7 +0,0 @@
-package vamk.e1700814.payapplication.api;
-
-public interface SessionInterface {
-    void initializeSession();
-    void onAuthenticationComplete(boolean success);
-    void layoutVisibility(boolean setVisible);
-}
